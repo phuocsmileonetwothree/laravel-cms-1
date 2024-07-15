c6c4ee193033bd06170668329c9334c43fdd73f9
phuocsmileonetwothree <94040178+phuocsmileonetwothree@users.noreply.github.com>
2024-07-16 00:23:26 +0700

    Update 2023_07_08_161543_create_cms_category_types_table.php

--- src/database/migrations/CMS/2023_07_08_161543_create_cms_category_types_table.php
+++ src/database/migrations/CMS/2023_07_08_161543_create_cms_category_types_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cms_resource_types', function (Blueprint $table) {
+        Schema::create('cms_category_types', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title');
             $table->text('desc')->nullable();
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cms_resource_types');
+        Schema::dropIfExists('cms_category_types');
     }
 };
